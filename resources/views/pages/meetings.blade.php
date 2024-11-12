0a68e95f4405b7e5adb6f2af58122ad7647491de
Phitsa <anthony.felipe.ff@gmail.com>
2024-11-12 16:56:51 -0300

    Create the form and index on the controller

--- resources/views/pages/meetings.blade.php
+++ resources/views/pages/meetings.blade.php
@@ -4,7 +4,22 @@
 
 @section('links', 'rel="stylesheet" href="https://cdn.jsdelivr.net/gh/habibmhamadi/multi-select-tag@3.1.0/dist/css/multi-select-tag.css"')
 
-
+@php
+    $members = [
+      [
+        'id'   => '1',
+        'name' => 'Anthony Felipe',
+      ],
+      [
+        'id'   => '2',
+        'name' => 'Luis Eduardo',
+      ],
+      [
+        'id'   => '3',
+        'name' => 'Ana Luisa',
+      ],
+];
+@endphp
 
 @section('MainContent')
 
@@ -17,15 +32,65 @@
 </section>
 <hr class="mx-8 border-1 border-black"/>
 
-<section>
-  <form id="registerMeetingForm" action="registermeeting">
+<section class="invisible fixed inset-0 flex flex-col justify-center items-center bg-gray-500 bg-opacity-50" id="registerMeetingForm">
+  <form class="bg-white rounded-lg w-1/4 p-6 grid grid-cols-2" id="closeModalMeetings" action="registermeeting">
 
-    <input type="text" name="meetingName" id="">
+    <h1 class="text-2xl mb-4 col-span-2">Create Member</h1>
+    
+    <label for="meetingName" class="text-md col-span-2">Meeting Name</label>
+    <input class="border-2 p-2 rounded-lg mb-4 col-span-2" type="text" name="meetingName" id="" placeholder="Meeting theme...">
 
+    <label for="meetingDate" class="col-span-2">Meeting Date</label>
+    <input class="border-2 rounded-lg p-2 mb-2 col-span-2" type="date" name="meetingDate" id="">
+
+    <div class="col-span-2 mb-2">
+      <label class="col-span-2" for="meetingMembers">Meeting Members</label>
+      <select name="meetingMembers" id="membersSelect" class="rounded-lg border-2 col-span-2" multiple id="">
+        @foreach ($members as $member)
+            <option value="{{$member['id']}}">{{$member['name']}}</option>
+        @endforeach
+      </select>
+    </div>
+
+    <label class="col-span-2" for="meetingDescription">Description</label>
+    <textarea class="col-span-2 border-2 mb-6 h-20 text-sm" name="meetingDescription" id=""></textarea>
+    
+    <div class="col-span-2 flex items-center justify-center">
+      <input type="submit" value="Send" class="bg-sky-400 cursor-pointer text-white font-bold col-span-2 rounded-md p-2 w-24">
+    </div>
   </form>
 </section>
 
 <script>
 
+  const openModalMeetings = document.getElementById('openModalMeetings')
+  const registerMeetingForm = document.getElementById('registerMeetingForm')
+
+
+  openModalMeetings.addEventListener('click', function() {
+    registerMeetingForm.style.visibility = 'visible';
+  })
+
+  registerMeetingForm.addEventListener('click', function(event) {
+    if(event.target === registerMeetingForm) {
+      document.getElementById('registerMeetingForm').style.visibility = 'hidden';
+      editToggleTransition.classList.remove('after:transition-all');
+    }
+  });
+</script>
+
+
+
+<script src="https://cdn.jsdelivr.net/gh/habibmhamadi/multi-select-tag@3.1.0/dist/js/multi-select-tag.js"></script>
+<script>
+  new MultiSelectTag('membersSelect', {
+    placeholder: "Search",
+    rounded: true,
+    tagColor: {
+        textColor: '#327b2c',
+        borderColor: '#92e681',
+        bgColor: '#eaffe6',
+    },
+  })
 </script>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MeetingController;
 use App\Http\Controllers\MemberController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,9 +8,7 @@ Route::get('/', function () {
     return view('pages/home');
 })->name('home');
 
-Route::get('/meetings', function () {
-    return view('pages/meetings');
-})->name('meetings.index');
+Route::get('/meetings', [MeetingController::class, 'index'])->name('meetings.index');
 
 Route::post('/savemember', [MemberController::class, 'store'])->name('member.store'); 
 
